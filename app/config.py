3f836c5fc1e3e714cb7c59fcc04e0823653da419
kwartoshka_VB <kwartoshka@gmail.com>
2021-12-01 22:18:34 +0300

    Done

--- app/config.py
+++ app/config.py
@@ -1,3 +1,3 @@
-POSTGRE_URI = 'postgresql://abobus:aboba@127.0.0.1:5431/flask_db'
+POSTGRE_URI = 'postgresql://abobus:aboba@172.18.0.1:5432/flask_db'
 SALT = 'fewrgn4ntreto875!^$*F$ETGGFDGsadsadas98R45TR'
 
